5f5af96a1094d580b9d1f3c67b8cf94a51866c4f
Tushar Bhatt <tusharbhatt0135@gmail.com>
2023-05-20 02:28:51 +0530

    none

--- src/My.js
+++ src/My.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function My() {
   return (
     <div className='none'>
-        <img src={myself} className=' sm:h-screen m-auto opacity-80 '/>
+        <img src={myself} className= 'sm:h-screen m-auto opacity-80 '/>
         <Link to={"/task"}><button  className=' text-black font-bold bg-red-500 p-5 absolute top-1/2 left-1/2 z-1 '>Continue &gt;</button></Link>
 
     </div>
